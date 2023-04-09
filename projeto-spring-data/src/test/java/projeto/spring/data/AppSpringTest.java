c02d8e5a6d82b87720f569b64c0e76ddc33440f5
Home <Home@DESKTOP-F8NUUGM>
2023-04-09 17:24:31 -0300

    criando o daoRepository e implementando crud no sistema

--- projeto-spring-data/src/test/java/projeto/spring/data/AppSpringTest.java
+++ projeto-spring-data/src/test/java/projeto/spring/data/AppSpringTest.java
@@ -2,12 +2,18 @@ package projeto.spring.data;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import projeto.spring.data.dao.InterfaceSpringDataUser;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
 public class AppSpringTest {
+	
+	@Autowired
+	private InterfaceSpringDataUser interfaceSpringDataUser;
 	
 	@Test 
 	public void testeInsert() {
